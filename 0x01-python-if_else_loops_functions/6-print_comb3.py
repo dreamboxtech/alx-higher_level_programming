56527aa06e4bc52ef5fddf379aefaf45c042fe95
dreamboxtech <bbbolaleye@gmail.com>
2022-09-20 23:15:00 +0100

    c

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # lst = ["{:02d}".format(a) for a in range(100)]
 for a in range(10):
-	for b in range(10):
-		if a >= b:
-			continue
-		elif a == 8 and b == 9:
-			print("{}{}".format(a,b))
-		else:
-			print("{}{}".format(a,b), end=", ")
+    for b in range(10):
+        if a >= b:
+            continue
+        elif a == 8 and b == 9:
+            print("{}{}".format(a, b))
+        else:
+            print("{}{}".format(a, b), end=", ")
