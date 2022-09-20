9ca57d303c70f5325588ecd7374d71426f6d0be1
dreamboxtech <bbbolaleye@gmail.com>
2022-09-21 00:11:45 +0100

    scan

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last = 0
+    if number > 0:
+        last = number%10
+    if number < 0:
+        last = -1 * (abs(number)%10)
+    print(last)
+    
