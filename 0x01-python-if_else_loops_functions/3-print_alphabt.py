8d374e62d3cbe09ed26e3c40a0ff927ec79252ef
dreamboxtech <bbbolaleye@gmail.com>
2022-09-20 20:56:39 +0100

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(["{}".format(chr(a)) for a in range(97, 123) if chr(a) not in 'eq']), end="")
+print("".join(["{}".format(chr(a)) for a in range(97, 123) 
+	if chr(a) not in 'eq']), end="")
