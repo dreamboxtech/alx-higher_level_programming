4c79bf39f9e79b508488bad11c21aeefc955cdb1
dreamboxtech <bbbolaleye@gmail.com>
2022-09-21 01:42:06 +0100

    rev

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-print(
- "".join([chr(i+32) if i % 2 == 0 else chr(i) for i in range(90, 64, -1)]),
- end=''
-)
+# print(
+#  "".join([chr(i+32) if i % 2 == 0 else chr(i) for i in range(90, 64, -1)]),
+#  end=''
+# )
+for i in range(90, 64, -1):
+    print("{}".format(chr(i+32)) if i % 2 == 0 else chr(i), end='')
