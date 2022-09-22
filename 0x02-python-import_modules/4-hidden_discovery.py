2d90cddb2550c8bddfbbeb5e93ca6eb729eead38
dreamboxtech <bbbolaleye@gmail.com>
2022-09-22 22:53:28 +0100

    4n5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ import hidden_4
 
 if __name__ == "__main__":
     res = [name for name in dir(hidden_4) if not name.startswith("__")]
-    print(*sorted(res), sep="\n")
+    print(*sorted(res), sep="\n", end="")
