bd43d40b8ddfa2f72a19c32d5c0fdeea21ea67f5
dreamboxtech <bbbolaleye@gmail.com>
2022-09-22 21:40:11 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,14 +7,14 @@ def run():
     length = len(args) - 1
 
     def list_args(args):
-        for index, value in enumerate(args):
-            print("{}: {}".format(index + 1, value))
+        for index, value in enumerate(args[1:]):
+            print("{}: {}".format(index, value))
 
-    if length == 1:
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
         print("{} argument:".format(length))
         list_args(args)
-    elif length == 0:
-        print("{} arguments.".format(length))
     elif length > 1:
         print("{} arguments:".format(length))
         list_args(args)
