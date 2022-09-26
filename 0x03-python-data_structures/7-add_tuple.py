046fe9e15e3fad94e3886d4a346b11fa58e8f99d
dreamboxtech <bbbolaleye@gmail.com>
2022-09-26 12:25:39 +0100

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if not tuple_a:
-        tuple_a = 0, 0
-    if not tuple_b:
-        tuple_b = 0, 0
-    try:
-        tuple_a[1]
-    except Exception:
-        tuple_a = tuple_a[0], 0
-    try:
-        tuple_b[1]
-    except Exception:
-        tuple_b = tuple_b[0], 0
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
     t1 = tuple_a[0] + tuple_b[0]
     t2 = tuple_a[1] + tuple_b[1]
