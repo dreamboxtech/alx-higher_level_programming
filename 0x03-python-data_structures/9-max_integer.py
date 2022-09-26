e7163373ee9838c915e41f066ab06a054b1b743b
dreamboxtech <bbbolaleye@gmail.com>
2022-09-26 13:06:15 +0100

    Mad max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if my_list:
         temp = my_list[0]
-        for num in my_list:
+        for num in my_list[1:]:
             if num > temp:
                 temp = num
         return temp
