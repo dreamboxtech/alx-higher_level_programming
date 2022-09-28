32bb2704405463bea69e74f7290ba1d1c5308d21
dreamboxtech <bbbolaleye@gmail.com>
2022-09-28 14:32:16 +0100

    sorted dict items

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    print(*[f"{k}: {v}" for k, v in sorted(a_dictionary.items())], sep="\n")
+    print(*(f"{k}: {v}" for k, v in sorted(a_dictionary.items())), sep="\n")
