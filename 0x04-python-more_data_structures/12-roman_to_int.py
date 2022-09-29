acdd6a4dcc598b407bb47d0dd74b9ae06b6b9ba6
dreamboxtech <bbbolaleye@gmail.com>
2022-09-29 09:53:42 +0100

    roman 2 Int?

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,12 +6,12 @@ def roman_to_int(roman_string):
     pairs = {"I": 1, "V": 5, "X": 10,
              "L": 50, "C": 100, "D": 500, "M": 1000}
 
-    c = [pairs[roman_string[i]] for i in range(len(roman_string))]
+    c = [*map(pairs.get, list(roman_string))]
     num = c[0]
     for i in range(len(c)-1):
         if c[i] >= c[i+1]:
             num += c[i+1]
         else:
-            num += (c[i+1] - c[i] - c[i])
+            num += (c[i+1] - (2*c[i]))
 
     return num
