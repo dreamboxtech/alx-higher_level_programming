60064cf7f0a2d1a2467f54c843d9947c34bd7302
dreamboxtech <bbbolaleye@gmail.com>
2022-09-29 16:27:00 +0100

    easy delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.items() if val != value}
+    a_dictionary = {k: v for k, v in a_dictionary.items() if v != value}
+    return a_dictionary
