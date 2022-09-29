baaa4949a4a6e6a91622953fbf2637df723085c1
dreamboxtech <bbbolaleye@gmail.com>
2022-09-29 16:53:05 +0100

    easy delete key

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    a_dictionary = {k: v for k, v in a_dictionary.items() if v != value}
+    for k, v in a_dictionary.copy().items():
+        if v == value:
+            a_dictionary.pop(k)
+
     return a_dictionary
