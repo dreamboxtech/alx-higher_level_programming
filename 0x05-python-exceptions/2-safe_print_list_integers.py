603e5b4096c20fd8a69547e6385a48c92c7b9287
dreamboxtech <bbbolaleye@gmail.com>
2022-10-10 20:59:39 +0100

    silently pass

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             c += 1
-        except IndexError:
+        except (IndexError, TypeError, ValueError):
             pass
     print("")
     return c
