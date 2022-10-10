fbb8f7171f560bae11f88f10d5c1f9ef36bb7afb
dreamboxtech <bbbolaleye@gmail.com>
2022-10-10 21:38:53 +0100

    safe div

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    res = int
     try:
         res = a/b
     except (TypeError, ValueError):
-        pass
+        res = None
     finally:
         print("Inside result: {}".format(res))
     return res
