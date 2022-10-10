a796b58cc80558e010dcb641a7b9203c34ed3324
dreamboxtech <bbbolaleye@gmail.com>
2022-10-10 20:50:10 +0100

    try it, except it

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     c = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
             c += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
     print("")
     return c
