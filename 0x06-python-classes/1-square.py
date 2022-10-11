4796a2d30563b2dba056669e9edfb228a581cda6
dreamboxtech <bbbolaleye@gmail.com>
2022-10-11 21:10:19 +0100

    private instance size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An empty 'class Square' here"""
+
+
+class Square:
+    """ A square is a four-sided shape"""
+    def __init__(self, size):
+        """Contructor set up goes here..."""
+        self.__size = size
