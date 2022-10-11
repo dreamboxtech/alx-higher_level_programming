1f6d890458c2fef9304880ac6dba33ec7b4370b7
dreamboxtech <bbbolaleye@gmail.com>
2022-10-11 22:31:56 +0100

    let's see it

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""An empty 'class Square' here"""
+
+
+class Square:
+    """ A square is a four-sided shape"""
+    def __init__(self, size=0):
+        """Contructor set up goes here..."""
+
+        self.size = size
+
+    @property
+    def size(self):
+        """This retrieves the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """A public intance method that
+        returns area of the square"""
+        return self.__size**2
+
+    def my_print(self):
+        """prints out the square"""
+
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
