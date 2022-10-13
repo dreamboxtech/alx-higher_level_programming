1846d9ee1a2cb11f706b6a263ba7c3b44d4147f7
dreamboxtech <bbbolaleye@gmail.com>
2022-10-14 00:28:56 +0100

    divide and conquer test

--- 0x07-python-test_driven_development/0-add_integer.py.py
+++ 0x07-python-test_driven_development/0-add_integer.py.py
@@ -5,11 +5,9 @@
 def add_integer(a, b=98):
     """Basic add function"""
     if type(a) not in [float, int]:
-        raise TypeError:
-            print("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) not in [float, int]:
-        raise TypeError:
-            print("b must be an integer")
+        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
     return (a+b)
