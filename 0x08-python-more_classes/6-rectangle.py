0a5de94169caa78c4a72afba25ea64b5616c779a
dreamboxtech <bbbolaleye@gmail.com>
2022-10-17 22:52:06 +0100

    counter part

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
 
         self.width = width
         self.height = height
-        number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -73,5 +73,5 @@ class Rectangle:
 
     def __del__(self):
         """Some other time"""
-        number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
