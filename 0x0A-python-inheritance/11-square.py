ba6037ca0df9df2877680375bffdf289785e6a49
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 04:14:18 +0100

    si

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -14,3 +14,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """Simply returns evaluated area"""
+        return self.__size * self.__size
