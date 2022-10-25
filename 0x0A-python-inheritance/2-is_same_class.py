1568cf4b62885b1319623447daed07d3f25304fd
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 02:20:00 +0100

    T/F type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that contains a class MyList that inherits from list
+"""
+
+
+def is_same_class(obj, a_class):
+    """Function that returns True or False based on type"""
+    return type(obj) == type(a_class)
