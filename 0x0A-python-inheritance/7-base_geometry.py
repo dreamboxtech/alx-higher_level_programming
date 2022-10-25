5c25c750a338562130629bad1370092df60718dc
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 03:15:15 +0100

    shapeit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Module that has an empty base class BaseGeometry and empty class function
+"""
+Contains the class BaseGeometry
 """
 
 
 class BaseGeometry:
-    """Class that defines a shape
-    """
+    """A class with public instance methods area and integer_validator"""
     def area(self):
-        """Function that calculates the area of a shape"""
+        """raises an exception when called"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Runs an evaluation on value and reponds with appropriate errors"""
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{:s} must be greater than 0".format(name))
