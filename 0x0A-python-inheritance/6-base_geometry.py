77357853bfbafb1b8abd2c28b99f2076bb3cce4a
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 02:56:09 +0100

    raise e

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""An empty class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """A non-empty geometry class"""
+
+    def area(self):
+        """method raises exception for now"""
+        raise Exception("area() is not implemeted")
