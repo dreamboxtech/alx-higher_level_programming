dc4981912bba9d97c6a8d14d6e0d510503db677d
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 04:38:52 +0100

    hasattr...

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+"""A function that adds attributes to objects."""
 
 
-class MyInt(int):
-    """Invert int operators == and !=."""
-
-    def __eq__(self, value):
-        """Override == opeartor with != behavior."""
-        return self.real != value
-
-    def __ne__(self, value):
-        """Override != operator with == behavior."""
-        return self.real == value
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
+    Args:
+        obj (any): The object to add an attribute to.
+        att (str): The name of the attribute to add to obj.
+        value (any): The value of att.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
