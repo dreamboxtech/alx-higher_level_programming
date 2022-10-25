7da470f4ba53b04a15b979985657bce7c3a38675
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 23:17:17 +0100

    readlines

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """This simple function accepts file name as arguement and prints
     out the content of the file"""
     with open(filename, encoding='utf-8') as f:
-        print(f.readlines(), end='')
+        print(f.readlines())
