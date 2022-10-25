9c7b47989418b2907720b150862dfc4062243d8f
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 02:28:25 +0100

    instance?

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that contains a class MyList that inherits from list
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that returns True or False based on type"""
+    return isinstance(obj, a_class)
