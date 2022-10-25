873b2e0ba55ed69a679f7d4fb2ed34906002fcd1
dreamboxtech <bbbolaleye@gmail.com>
2022-10-26 00:17:00 +0100

    scripted

--- 0x0B-python-input_output/5-save_to_json_file - Copy.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""A a simple example of reading from a file"""
-import json
-
-
-def from_json_string(my_str):
-    """ a function that returns an object (Python data structure)
-    represented by a JSON string:"""
-    return json.loads(my_str)
