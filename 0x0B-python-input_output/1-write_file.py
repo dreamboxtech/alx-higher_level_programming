59abf553c4cc9a4843587a17f985885ea8cab8ac
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 23:23:19 +0100

    w mode

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A a simple example of reading from a file"""
+
+
+def write_file(filename="", text=""):
+    """A function that writes a string to a text file (UTF8)
+    and returns the number of characters written"""
+    with open(filename, encoding='utf-8', mode='w') as f:
+        f.write(text)
+    return len(text)
