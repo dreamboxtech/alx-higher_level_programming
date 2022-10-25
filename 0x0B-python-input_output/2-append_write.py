021d97c01af84de7e9003221dd7568412e1486d8
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 23:31:42 +0100

    a mode

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """A a simple example of reading from a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """A function that writes a string to a text file (UTF8)
     and returns the number of characters written"""
     with open(filename, encoding='utf-8', mode='a') as f:
