9a3144c4922bdf1c7c28133f044ed5047c9a23fd
dreamboxtech <bbbolaleye@gmail.com>
2022-10-25 01:32:00 +0100

    return dir(obj)

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that returns attributes and methods in a class
+"""
+
+
+def lookup(obj):
+    """This functions returns methods and 
+    attributes of an object.
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+# alx-higher_level_programming
+
+### Now working on inheritance in python OOP
